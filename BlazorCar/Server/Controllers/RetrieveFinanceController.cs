4ce89e65e5c481f9f0a9dab4ad572148156909e5
AaronEki <aaron.connolly61@gmail.com>
2023-03-04 19:45:13 +0000

    admin module - working - potential for improvement down the line

---
+++ BlazorCar/Server/Controllers/RetrieveFinanceController.cs
@@ -0,0 +1,28 @@
+﻿using BlazorCar.Server.Data;
+using BlazorCar.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCar.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RetrieveFinanceController : ControllerBase
+    {   
+        //used for getting data form the database! This is using the database in DataContext
+        private readonly DataContext _dbContext;
+
+        public RetrieveFinanceController(DataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        //method to get the contects of the Customers table in the database and return this list
+        public async Task<ActionResult<IEnumerable<Customer>>> RetrieveFinance()
+        {
+            return await _dbContext.Customers.ToListAsync();
+        }
+    }
+}
--- BlazorCar/Server/Services/CustomerService/CustomerService.cs
+++ BlazorCar/Server/Services/CustomerService/CustomerService.cs
@@ -12,17 +12,23 @@ namespace BlazorCar.Server.Services.CustomerService
         {
             _dbContext = dbContext;
         }
+
+        //method which returns a boolean and takes parameters object customer of type Customer (defined Customer.cs)
         public async Task<bool> AddCustomer(Customer customer)
         {          
             try
             {
+                //adding to the customers table - the customer object passed as a parameter into this funciton
                 _dbContext.Customers.Add(customer);
+                //saving the changes to the db
                 await _dbContext.SaveChangesAsync();
 
+                //return true on success
                 return true;
             }
             catch
             {
+                //return false on fail
                 return false;
             }
 
